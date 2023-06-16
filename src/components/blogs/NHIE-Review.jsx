55f4a8606721f0bc5760f69be187f8b30985ff1b
Ashwin <24ashwinv@gmail.com>
2023-06-16 12:47:46 -0500

    blog update nhie

--- src/components/blogs/NHIE-Review.jsx
+++ src/components/blogs/NHIE-Review.jsx
@@ -27,12 +27,12 @@ export default function NHIEReview() {
           </ul>
         <br />
         The pitiful attempts at showing Indian culture throughout the show just shows the lack of knowledge 
-        from Mindy Kaling and the writers behind the scenes. A quick example is when Nalini goes to India.
-        Her mom says to the maid/housekeeper to get some "paruppu," meaning lentils in English. 
-        The problem is, you don't eat parrupu by itself. You combine it with other dishes. Although this 
-        is a picky example, there are much more broader examples of the lack of true Indian/Tamil culture. 
-        For example, other than Nalini rarely, no one even speaks Tamil. Even when Nalini speaks to her own 
-        mother they speak in English.
+        from Mindy Kaling and the writers behind the scenes. A quick example I can point out throughout the 
+        show is that other than Nalini every once in a while, no one even speaks Tamil. Even when Nalini speaks 
+        to her own mother when visiting India, they speak in English. I get that this is for the audience watching,
+        but I believe adding more Tamil dialogue will add to the realism of the show. This just shows that the writers
+        seem incompetent in understanding Tamil culture. Again there are many more instances of shoddy attempts to show
+        Indian culture.
         <br />
         <br />
         One of the biggest problems with this show is how Devi seems to hate her culture completely. 
@@ -51,7 +51,8 @@ export default function NHIEReview() {
         In addition to this, under all of the drama, Devi is just a narcissistic brat who uses people and 
         takes her anger out on others when things don't go her way. A lot of times I was never really rooting 
         for Devi. She gets jealous for the smallest things and bases her value on how many dudes she can get 
-        with (as seen with juggling both Ben and Paxton in the earlier seasons)
+        with (as seen with juggling both Ben and Paxton in the earlier seasons). It's fine to be morally ambiguous,
+        but it is another thing to make out your character as a brat who hasn't really learned anything.
         <br />
         <br />
         Another thing that ties along with Kaling's obsession of Indians trying to be with white/non-Indian people. 
