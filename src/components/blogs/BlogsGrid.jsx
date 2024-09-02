7045b51968d341f5f0c0a1b70b76bb65b6817472
Ashwin <24ashwinv@gmail.com>
2024-09-01 22:51:28 -0500

    changed blogs grid format

--- src/components/blogs/BlogsGrid.jsx
+++ src/components/blogs/BlogsGrid.jsx
@@ -3,8 +3,8 @@ import BlogCard from './BlogCard';
 
 const BlogsGrid = ({blogs}) => {
     return(
-        <div className="p-6">
-            <div className="grid grid-cols-1 gap-8 text-center overflow-y-hidden mx-auto ">
+        <div className="p-6 mt-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 text-center overflow-y-hidden mx-auto">
             {blogs.map((blog, index) => (
                 <BlogCard key={index} blog={blog} />
             ))}
